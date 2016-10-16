f2061f3352c0d4b0d5eab21a3ccaa4368cf4716a
ian Wahbe <ian@wahbe.com>
2016-10-16 01:32:49 -0700

    Recursive Functions
    can’t get listReverse

--- RecursiveFunctions.py
+++ RecursiveFunctions.py
@@ -26,13 +26,18 @@ print "fib(5) == 5 = ",fib(5)," : ", 5 == fib(5)
 print "fib(11) == 89 = ",fib(11)," : ", 89 == fib(11)
 print "fibIter(5) == 5 = ",fibIter(5)," : ", 5 == fibIter(5)
 print "fibIter(11) == 89 = ",fibIter(11)," : ", 89 == fibIter(11)
-
+invl=[]
 def listReverse(L):
-    list=[]
-    len(L)
+    if len(L)==0:
+        return invl
+    else:
+        list.append(invl,L[-1])
+        L=L[0:-1]
+        listReverse(L)
+    return invl
 
+def listReverseIter(L):
+    return L[-1::-1]
 
-
-
-#print "listReverse([1,2,3,4]) == [4,3,2,1] = ",listReverse([1,2,3,4])," : ",listReverse([1,2,3,4]) == [4,3,2,1]
-#print "listReverseIter([1,2,3,4]) == [4,3,2,1] = ",listReverseIter([1,2,3,4])," : ",listReverseIter([1,2,3,4]) == [4,3,2,1]
+print "listReverse([1,2,3,4]) == [4,3,2,1] = ",listReverse([1,2,3,4])," : ",listReverse([1,2,3,4]) == [4,3,2,1]
+print "listReverseIter([1,2,3,4]) == [4,3,2,1] = ",listReverseIter([1,2,3,4])," : ",listReverseIter([1,2,3,4]) == [4,3,2,1]
--- Testing.py
+++ Testing.py
@@ -1,17 +1,23 @@
 import sys;
 print (sys.version)
-
+import time
+import calendar
 import random
-low1='false'
-low='false'
-while low1.isdigit()==False:
-    try:
-        low=raw_input('What is the low for "m" and "b"?')
-        low=int(low)
-        low1=abs(low)
-        low1=str(low1)
-    except:
-        pass
-
-print low
-print low1
+x=raw_input('how many times?')
+x=int(x)
+t=0
+u=0
+st=time.gmtime()
+st=calendar.timegm(st)
+for i in range (x):
+    a=2
+    p1=random.randint(0,1)
+    p2=random.randint(0,1)
+    if p1==p2:
+        t+=1
+    else:
+        u+=1
+print 'number of seconds needed to calculate:',calendar.timegm(time.gmtime())-st
+print 'same gender groups =',t,'/',x
+print 'defrent gender groups =',u,'/',x
+print 'ration of same:',float(t)/x,'different is:',float(u)/x
